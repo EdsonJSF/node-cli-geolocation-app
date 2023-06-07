4b5d295bd97244f2f0e425576d56b374dd749950
EdsonJSF <edsonjsf1990@gmail.com>
2023-06-07 08:49:03 -0400

    Class Search

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { inquirerMenu, pause, readInput } = require("./helpers/inquirer");
+const Search = require("./models/search");
 
 const main = async () => {
+  const search = new Search();
   let opt = "";
+
   do {
     opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        const input = await readInput("Ciudad: ");
+        search.city(input);
+        break;
+    }
+
     if (opt !== 0) await pause();
   } while (opt !== 0);
 };
---
+++ models/search.js
@@ -0,0 +1,13 @@
+class Search {
+  constructor() {
+    this.history = ["Madrid", "San José"];
+  }
+
+  async city(place = "") {
+    console.log(place);
+
+    return [];
+  }
+}
+
+module.exports = Search;
